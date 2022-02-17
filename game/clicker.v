7446ab5240ec43472bcdbb180492d520d116c072
mrsherobrine <mrsherobrinenaomi@gmail.com>
2022-02-17 01:39:19 +0100

    rewrite bigint formatter

--- game/clicker.v
+++ game/clicker.v
@@ -2,6 +2,8 @@ module game
 
 import math.big
 
+const magnitudes = ['', 'K', 'M', 'B', 'T', 'Qa', 'Qu', 'Sx', 'Sp', 'Oc', 'No', 'Dc', 'UDc', 'DDc']
+
 fn increment_credits() {
 	state.credits.inc()
 }
@@ -10,16 +12,21 @@ pub fn format_number_string(number big.Integer) string {
 
 	nr := number.str()
 
-	match nr.len {
-		// thousands
-		4 { return "${nr[0].ascii_str()}.${nr[1..4]}K"}
-		5 { return "${nr[0..2]}.${nr[2..4]}K"}
-		6 { return "${nr[0..3]}.${nr[3].ascii_str()}K" }
-		// millions
-		7 { return "${nr[0].ascii_str()}.${nr[1..4]}M"}
-		8 { return "${nr[0..2]}.${nr[2..4]}M"}
-		9 { return "${nr[0..3]}.${nr[3].ascii_str()}M"}
+	if nr.len > 3 {
 
-		else { return nr }
+		if nr.len % 3 == 1 {
+			return '${nr[0].ascii_str()}.${nr[1..4]}${magnitudes[nr.len/3]}'
+		}
+
+		if nr.len % 3 == 2 {
+			return '${nr[0..2]}.${nr[2..4]}${magnitudes[nr.len/3]}'
+		}
+
+		if nr.len % 3 == 0 {
+			return '${nr[0..3]}.${nr[3].ascii_str()}${magnitudes[nr.len/3]}'
+		}
+		
 	}
+
+	return nr
 }
